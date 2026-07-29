47302b796eb5f2c6cd5dae83b013a0af0621e6a1
Abdullah Sheikh <abdullahsheikh2608@gmail.com>
2026-07-29 17:10:38 +0500

    feat: configure Prisma and database migrations

---
+++ server/prisma/migrations/20260729120551_add_category_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
